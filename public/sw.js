8d53fdaf143c4d9a0831f64e679859e57da3624f
asaldanam <asaldanam@gmail.com>
2022-04-22 21:11:58 +0200

    service worker fix

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,13 @@
 //public/sw.js
 self.addEventListener("install", App);
 
-// (() => {
-//   // polling
-//   setInterval(notify, 5000)
-// })
+function App() {
+  // initPolling();
+}
+
+function initPolling() {
+  setInterval(notify, 5000)
+}
 
 function notify() {
   self.registration.showNotification(new Date().toISOString());
